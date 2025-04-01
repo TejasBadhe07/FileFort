c027538ddab7feb7e2890c94a0d286b777dd0f88
TejasBadhe07 <tbadhe75@gmail.com>
2025-04-01 16:23:05 +0530

    update in app.py

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../backend/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(app)
 
 # ✅ Define User Model
